5bb2f2c8d7b42e2fa82a44dc0c1664dff055cffd
Cassandra Bub <cassiebub@tamu.edu>
2018-12-21 12:08:09 -0800

    Image Changes

--- main.ts
+++ main.ts
@@ -1,50 +1,35 @@
 namespace projectImages {
     //% fixedInstance
-    export const Boss_Final = image.ofBuffer(hex`e4101000e000000000000000e20e00000000000020f200000000f000002e0e000000ff0000202e222200ff0f0000224422f2ff0f000020bb22feff000020222222fe0f000020222222fe0f00000020bb22feff000000224422f2ff0f00202e222200ff0f002e0e000000ff0020f200000000f000e20e000000000000e000000000000000`);
+    export const Boss_2 = image.ofBuffer(hex`e4101000000000000000000000000000f0ff0f0000f0ffffff550f0000ff4444f455ff0f004f444444554ffff04f4444445545f4f044111144554544f044111f44554544f044f11f44554544f044f11f44554544f04f4444445545f4004f444444554fff00ff4444f455ff0f00f0ffffff550f0000000000f0ff0f000000000000000000`);
     //% fixedInstance
-    export const Boss_Final_1 = image.ofBuffer(hex`e4101000ef0f000000000000eeff000000000000ffeeffffff0f0000f0eeefeeee0fffff00ffeeeeee0fffff00f0eeffe1ffff0f00f0efffe1feff0000efeeeeeefe0f0000efeeeeeefe0f0000f0efffe1feff0000f0eeffe1ffff0f00ffeeeeee0ffffff0eeefeeee0fffffffeeffffff0f0000eeff000000000000ef0f000000000000`);
+    export const Boss_Final = image.ofBuffer(hex`e4101000ef0f000000000000eeff000000000000ffeeffffff0f0000f0eeefeeee0fffff00ffeeeeee0fffff00f0eeffe1ffff0f00f0efffe1feff0000efeeeeeefe0f0000efeeeeeefe0f0000f0efffe1feff0000f0eeffe1ffff0f00ffeeeeee0ffffff0eeefeeee0fffffffeeffffff0f0000eeff000000000000ef0f000000000000`);
     //% fixedInstance
-    export const Boy = image.ofBuffer(hex`e41010000000000000000000000000606606000000000060660600000000006066060000dddddd6066060000ddbcdd6066666666ddcfdd6d66666666dddddd6d66666666dddddd6d66666666ddcfdd6d66666666ddbcdd6066666666dddddd60660600000000006066060000000000606606000000000060660600000000000000000000`);
+    export const Boy = image.ofBuffer(hex`e410100000000000ff0f0000000000f066ff0000000000f066f60000ffffffff66f60000dfddddff66f6ff0fdf1fddfd666666ffdfffdddd666666f6dfdddddd666666f6dfdddddd666666f6dfffdddd666666f6df1fddfd666666ffdfddddff66f6ff0fffffffff66f60000000000f066f60000000000f066ff000000000000ff0f0000`);
     //% fixedInstance
-    export const Boy1 = image.ofBuffer(hex`e410100000000000ff0f0000000000f066ff0000000000f066f60000ffffffff66f60000dfddddff66f6ff0fdf1fddfd666666ffdfffdddd666666f6dfdddddd666666f6dfdddddd666666f6dfffdddd666666f6df1fddfd666666ffdfddddff66f6ff0fffffffff66f60000000000f066f60000000000f066ff000000000000ff0f0000`);
+    export const Chest_1 = image.ofBuffer(hex`e4101000ffffffffffffffffefeeeeefeeeeeefeefeeeeefceeceefeefccccefeecceefeefeefeffefceecfeefee5f55f5eeeefeefee5ff555efeefeefcc5ff55ff5eefeefee5ff55ff5ccfcefee5ff555cfeefeefee5f55f5eeeefeefccfeffefeeeefeefeecccfeeeeeefcefeeeeefeeccccfeefeeeeefeeeeeefeffffffffffffffff`);
     //% fixedInstance
-    export const Chest = image.ofBuffer(hex`e41010000000000000000000000000000000000000e0eeeeeeeb0e0000eeeeeeeeebee0000eeeeeeeeebee0000eebeeceecbee0000eeceeeeeeeee0000eeee4ee4ebee0000eeee4e44ebee0000eeee4ee4ebee0000bbbbbebbbbbb0000eeceeeeecbee0000eeeeeeeeebee0000e0eeeeeeeb0e0000000000000000000000000000000000`);
+    export const Chest = image.ofBuffer(hex`e4101000ffffffffffffffffefeeeeefeeeeeefeefeeeeefeeeeeefeefeeeeefeeeeeefeefeefeffefeeeefeefee5f55f5eeeefeefee5ff555efeefeefee5ff55ff5eefeefee5ff55ff5eefeefee5ff555efeefeefee5f55f5eeeefeefeefeffefeeeefeefeeeeefeeeeeefeefeeeeefeeeeeefeefeeeeefeeeeeefeffffffffffffffff`);
     //% fixedInstance
-    export const Door = image.ofBuffer(hex`e410100000000000000000000000000000f0000000eeeefeeefeefee00eeeefeeefeefee00bbbbfebecbbcbb00eeeefeeeceecee00eeeefeeefeefee00eeeefeeefeefee00eeeefeeefeefee00eeeefefefeefee00bbbbfefeccbcbb00eeeefeeeceecee00eeeefeeefeefee00eeeeffeefeefee00000000000000000000000000000000`);
+    export const Flag = image.ofBuffer(hex`e4101000000000000000ffff0000000000005ff50000000000005ff5ffffffffffff5ff55f555555555555f55f555555555555f5ff555555f4ff5ff5f0555555ff005ff5f05f55450f005ff5005f55450f005ff5005f55f40f005ff500ff55f400005ff400f0550f00004ff400f05f0f0000ffff0000ff0f000000000000000000000000`);
     //% fixedInstance
-    export const Girl = image.ofBuffer(hex`e41010000000000000000000000000303303000000000030330300000000003033030000bddddd3033030000bdbcdd3033333333bdcfdd3d33333333dddddd3d33333333dddddd3d33333333bdcfdd3d33333333bdbcdd3033333333bddddd30330300000000003033030000000000303303000000000030330300000000000000000000`);
+    export const Flower = image.ofBuffer(hex`e4101000005f55ff00000000005f33f50f000000005f7345ff00000000ff5544ff0f0000ffffff4f44ff00005ff5ffff44f4ff0f3f535f44ff4444ff3537354544444ff4353753f444ff44ff5f7335ffff44f40f5f33ff5f44f4ff00ff55ff3545ff0000f0ffff53f30f000000005f33550f000000005f37530f00000000ff5fff0f0000`);
     //% fixedInstance
-    export const Girl1 = image.ofBuffer(hex`e410100000000000ff0f0000000000f033ff0000000000f033f30000ffffffff33f30000dfddddff33f3ff0fdf1fddfd333333ffdfffdddd333333f3dfdddddd333333f3dfdddddd333333f3dfffdddd333333f3df1fddfd333333ffdfddddff33f3ff0fffffffff33f30000000000f033f30000000000f033ff000000000000ff0f0000`);
+    export const Gate = image.ofBuffer(hex`e410100000ff0f0000f0ff00f0ffffffffffff0fff0ff00ff00ff0ffff0000ffff0000ff0ff00ff00ff00ff0ffffff0000ffffff0fff00ffff00fff00f000000000000f00f000000000000f00fff00ffff00fff0ffffff0000ffffff0ff00ff00ff00ff0ff0000ffff0000ffff0ff00ff00ff0fff0ffffffffffff0f00ff0f0000f0ff00`);
     //% fixedInstance
-    export const Border = image.ofBuffer(hex`e4101000dc11db1111bd11cdcd1dbd1dd1dbd1dcd1dcd1dccd1dcd1d11cd1dbddbd1dc11dbd1dcd11dcd1dbdbd1dcd1dd1dcd1dbd1dcd1dccd1dcd1d11bd1dfddfd1db1111bd1dfddfd1db11d1dcd1dccd1dcd1dbd1dcd1dd1dcd1dbdbd1dcd11dcd1dbd11cd1dbddbd1dc11d1dcd1dccd1dcd1dcd1dbd1dd1dbd1dcdc11db1111bd11cd`);
-    //% fixedInstance
-    export const Boss_1 = image.ofBuffer(hex`e410100000000000000000000000000000000000000000000000000000b0bbbbbb00000000b0dcbbbb00000000bbcfbbbb0c000000bbccbbbbfc000000bbbbbbbb0d000000bbbbbbbb0d000000bbccbbbbfc000000bbcfbbbb0c000000b0dcbbbb00000000b0bbbbbb000000000000000000000000000000000000000000000000000000`);
-    //% fixedInstance
-    export const Boss_1_1 = image.ofBuffer(hex`e4101000000000000000000000000000000000000000ffff0f00000000f0bbbbfb00000000bf1fbbbbffff0000bfffbbfbbbfb00f0bbffbb1bffff00f0bbbbbb1bf10000f0bbbbbb1bf10000f0bbffbb1bffff0000bfffbbfbbbfb0000bf1fbbbbffff0000f0bbbbfb0000000000ffff0f00000000000000000000000000000000000000`);
-    //% fixedInstance
-    export const Boss_2 = image.ofBuffer(hex`e4101000000000000000000000000000000000000000000000550000004044444455000000404444445544040044dddd445545440044111144554544004411db445545440044cddf445545440044bddb445545440044dddd4455454400404444445544040040444444554000000000000055000000000000000000000000000000000000`);
-    //% fixedInstance
-    export const Boss_2_1 = image.ofBuffer(hex`e4101000000000000000000000000000f0ff0f0000f0ffffff550f0000ff4444f455ff0f004f444444554ffff04f4444445545f4f044111144554544f044111f44554544f044f11f44554544f044f11f44554544f04f4444445545f4004f444444554fff00ff4444f455ff0f00f0ffffff550f0000000000f0ff0f000000000000000000`);
-    //% fixedInstance
-    export const Boss_3 = image.ofBuffer(hex`e41010000000000000000000000000000000000000080000000000000088000090990900008808009099090080b88c8896990900808888880800000080888888969909008088888896990900808888880800000080b88c889699090000880800909909000088000090990900000800000000000000000000000000000000000000000000`);
-    //% fixedInstance
-    export const Stone = image.ofBuffer(hex`e410100000f0ffffffff0f0000ffbbbbbfbbff00f0bfbbfbbfbbfb0fffbbfbbbbbbfbbffbfbbfbbbbbbffbfbbfffffbbffbffbffbffbbbbbbbbfbbffbfbbbbbbbbffbbfbbfbbfbbbbbbbbbfbbfbbbbffbfbbfffbbfffbbbbbfbbbffbbfbbbfbbbbbfbffbffbbffbbbbfbbbfff0bffbffbbbbfb0f00ffbbbbbbbbff0000f0ffffffff0f00`);
-    //% fixedInstance
-    export const Stone1 = image.ofBuffer(hex`e410100000ffffffffffff00f0dfddddddddfd0fffdddbdbdbbdddffdfbddfddfddddbfddfddbbbfdddbfdfdbfbdddddddddddfddfdfdfddddddbffddfdddbbddfdbddfddfdddddddfddbffdbfddddddddbdfdfddfbfdbbddfdddbfddfdbddfdbbddddfbdffddbddddffddfdffddfdbddbbdddfff0dfddddddddfd0f00ffffffffffff00`);
-    //% fixedInstance
-    export const Flower = image.ofBuffer(hex`e4101000005f55ff00000000005f33f50f000000005fe365ff00000000ff5566ff0f0000ffffff6f66ff00005ff5ffff66f6ff0f3f535f66ff6666ff353e356566666ff6353e53f666ff66ff5fe335ffff66f60f5f33ff5f66f6ff00ff55ff3565ff0000f0ffff53f30f000000005f33550f000000005f3e530f00000000ff5fff0f0000`);
-    //% fixedInstance
-    export const Chest1 = image.ofBuffer(hex`e4101000ffffffffffffffffefeeeeefeeeeeefeefeeeeefceeceefeefccccefeecceefeefeefeffefceecfeefee5f55f5eeeefeefee5ff555efeefeefcc5ff55ff5eefeefee5ff55ff5ccfcefee5ff555cfeefeefee5f55f5eeeefeefccfeffefeeeefeefeecccfeeeeeefcefeeeeefeeccccfeefeeeeefeeeeeefeffffffffffffffff`);
-    //% fixedInstance
-    export const Gate = image.ofBuffer(hex`e410100000f00f0000f00f0000ffff0000ffff00f00ff00ff00ff00fff0000ffff0000ff0ff00ff00ff00ff0ffffff0000ffffff0fff00ffff00fff00f000000000000f00f000000000000f00fff00ffff00fff0ffffff0000ffffff0ff00ff00ff00ff0ff0000ffff0000fff00ff00ff00ff00f00ffff0000ffff0000f00f0000f00f00`);
-    //% fixedInstance
-    export const Gate1 = image.ofBuffer(hex`e410100000ff0f0000f0ff00f0ffffffffffff0fff0ff00ff00ff0ffff0000ffff0000ff0ff00ff00ff00ff0ffffff0000ffffff0fff00ffff00fff00f000000000000f00f000000000000f00fff00ffff00fff0ffffff0000ffffff0ff00ff00ff00ff0ff0000ffff0000ffff0ff00ff00ff0fff0ffffffffffff0f00ff0f0000f0ff00`);
-    //% fixedInstance
-    export const Chest1_1 = image.ofBuffer(hex`e4101000ffffffffffffffffefeeeeefeeeeeefeefeeeeefeeeeeefeefeeeeefeeeeeefeefeefeffefeeeefeefee5f55f5eeeefeefee5ff555efeefeefee5ff55ff5eefeefee5ff55ff5eefeefee5ff555efeefeefee5f55f5eeeefeefeefeffefeeeefeefeeeeefeeeeeefeefeeeeefeeeeeefeefeeeeefeeeeeefeffffffffffffffff`);
+    export const Girl = image.ofBuffer(hex`e410100000000000ff0f0000000000f033ff0000000000f033f30000ffffffff33f30000cfccccff33f3ff0fcf1fccfc333333ffcfffcccc333333f3cfcccccc333333f3cfcccccc333333f3cfffcccc333333f3cf1fccfc333333ffcfccccff33f3ff0fffffffff33f30000000000f033f30000000000f033ff000000000000ff0f0000`);
     //% fixedInstance
     export const Grass = image.ofBuffer(hex`e41010000000f0ff000000000000f0f6000000000000f0f6ff0000000000f066f60f000000f0ff6fd6ff0000ffff666666fdff0fff6666ff6f66660f6fd6fdffff6fff0f6ffdffff66660f00ffff006f66ff0f000000f06ff60f00000000ff66ff00000000006ff60f00000000006ffd000000000000ffff000000000000000000000000`);
     //% fixedInstance
-    export const Flag = image.ofBuffer(hex`e4101000000000000000ffff0000000000005ff50000000000005ff5ffffffffffff5ff55f555555555555f55f555555555555f5ff555555f4ff5ff5f0555555ff005ff5f05f55450f005ff5005f55450f005ff5005f55f40f005ff500ff55f400005ff400f0550f00004ff400f05f0f0000ffff0000ff0f000000000000000000000000`);
+    export const Stone_1 = image.ofBuffer(hex`e410100000f0ffffffff0f0000ffbbbbbfbbff00f0bfbbfbbfbbfb0fffbbfbbbbbbfbbffbfbbfbbbbbbffbfbbfffffbbffbffbffbffbbbbbbbbfbbffbfbbbbbbbbffbbfbbfbbfbbbbbbbbbfbbfbbbbffbfbbfffbbfffbbbbbfbbbffbbfbbbfbbbbbfbffbffbbffbbbbfbbbfff0bffbffbbbbfb0f00ffbbbbbbbbff0000f0ffffffff0f00`);
+    //% fixedInstance
+    export const Stone = image.ofBuffer(hex`e410100000ffffffffffff00f0dfddddddddfd0fffddd8d8d88dddffdf8ddfddfdddd8fddfdd888fddd8fdfd8f8dddddddddddfddfdfdfdddddd8ffddfddd88ddfd8ddfddfdddddddfdd8ffd8fdddddddd8dfdfddf8fd88ddfddd8fddfd8ddfd88ddddfddffdd8ddddffddfdffddfd8dd88dddfff0dfddddddddfd0f00ffffffffffff00`);
+    //% fixedInstance
+    export const Boss_1 = image.ofBuffer(hex`e4101000000000000000000000000000000000000000ffff0f00000000f0bbbbfb00000000bf1fbbbbffff0000bfffbbfbbbfb00f0bbffbb1bffff00f0bbbbbb1bf10000f0bbbbbb1bf10000f0bbffbb1bffff0000bfffbbfbbbfb0000bf1fbbbbffff0000f0bbbbfb0000000000ffff0f00000000000000000000000000000000000000`);
+}
+
+namespace palettes {
+    //% fixedInstance
+    export const palettehex = hex`000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`;
 }
 
 enum Kind {
@@ -105,11 +90,11 @@ function levelsSetup() {
         . . . . . . . . . . . . . . . . . . . . . . . 9 . . . . . . . . . . . . . . . . 9 . . . . . . . . . . . . . . 9
         . . . . . . . . . . . . . . . . . . . . . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
         `)
-    scene.setTile(9, projectImages.Stone1, true)
-    scene.setTile(6, projectImages.Gate1)
-    scene.setTile(3, projectImages.Boy1)
+    scene.setTile(9, projectImages.Stone, true)
+    scene.setTile(6, projectImages.Gate)
+    scene.setTile(3, projectImages.Boy)
     scene.setTile(2, projectImages.Flower)
-    scene.setTile(1, projectImages.Grass)
+    //scene.setTile(1, projectImages.Grass)
     scene.setTile(4, projectImages.Flag)
     boySprite.setPosition(398, 268)
     girlSprite.setPosition(30, 200)
@@ -179,27 +164,27 @@ sprites.onOverlap(Kind.Girl, Kind.Boy, function (mainSprite, hitSprite) {
     game.showLongText("Your time to complete was: " + game.runtime() + "ms. Thanks for playing!", DialogLayout.Center)
 })
 
-introSequence()
+//introSequence()
 
-let boySprite = sprites.create(projectImages.Boy1, Kind.Boy)
-let girlSprite = sprites.create(projectImages.Girl1, Kind.Girl)
-let boss1Sprite = sprites.create(projectImages.Boss_1_1, Kind.Boss)
-let boss2Sprite = sprites.create(projectImages.Boss_2_1, Kind.Boss)
-let bossFinalSprite = sprites.create(projectImages.Boss_Final_1, Kind.Boss)
-let chest1Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest2Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest3Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest4Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest5Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest6Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest7Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest8Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest9Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest10Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest11Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
-let chest12Sprite = sprites.create(projectImages.Chest1_1, Kind.Chest)
+let boySprite = sprites.create(projectImages.Boy, Kind.Boy)
+let girlSprite = sprites.create(projectImages.Girl, Kind.Girl)
+let boss1Sprite = sprites.create(projectImages.Boss_1, Kind.Boss)
+let boss2Sprite = sprites.create(projectImages.Boss_2, Kind.Boss)
+let bossFinalSprite = sprites.create(projectImages.Boss_Final, Kind.Boss)
+let chest1Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest2Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest3Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest4Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest5Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest6Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest7Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest8Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest9Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest10Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest11Sprite = sprites.create(projectImages.Chest, Kind.Chest)
+let chest12Sprite = sprites.create(projectImages.Chest, Kind.Chest)
 
-scene.setBackgroundColor(7)
+scene.setBackgroundColor(9)
 levelsSetup()
 let chestsCollected = 0
 
